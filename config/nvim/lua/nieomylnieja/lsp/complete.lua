28054725e7d616831705c4d28550278244c89b29
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2022-09-20 16:54:42 +0200

    navigate varible snippets args

--- config/nvim/lua/nieomylnieja/lsp/complete.lua
+++ config/nvim/lua/nieomylnieja/lsp/complete.lua
@@ -32,7 +32,6 @@ cmp.setup({
 		end,
 	},
 	mapping = cmp.mapping.preset.insert({
-		-- ["<tab>"] = cmp.mapping.confirm({ select = true }),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
--- config/nvim/lua/nieomylnieja/lsp/snippets.lua
+++ config/nvim/lua/nieomylnieja/lsp/snippets.lua
@@ -1,3 +1,12 @@
+-- Jump between input parameters for snippets
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+keymap("i", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+keymap("s", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+keymap("i", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
+keymap("s", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
+
+-- Load friendly snippets
 require("luasnip.loaders.from_vscode").lazy_load({
 	include = nil, -- Load all languages
 })
