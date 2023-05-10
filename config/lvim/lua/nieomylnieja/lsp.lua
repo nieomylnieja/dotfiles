ebe97ebeb0a2394bc42bdd8e2bdb635c2d15d161
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2023-05-10 22:46:47 +0200

    move old nvim conf and add lvim funcs

---
+++ config/lvim/lua/nieomylnieja/lsp.lua
@@ -0,0 +1,43 @@
+lvim.lsp.installer.setup.automatic_installation = false
+
+lvim.lsp.installer.setup.ensure_installed = {
+  "lua_ls",
+  "jsonls",
+  "yamlls",
+  "gopls",
+  "bashls",
+  "pylsp",
+  "terraformls",
+  "dockerls",
+  "tsserver",
+  "taplo",
+  "ansiblels"
+}
+
+-- Configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+local manager = require("lvim.lsp.manager")
+local server_overrides = {
+  ["taplo"] = { filetypes = { "toml" } }
+  -- ["terraformls"] = { filetypes = { "terraform", "terraform-vars", "hcl" } }
+}
+for server, opts in pairs(server_overrides) do
+  table.insert(lvim.lsp.automatic_configuration.skipped_servers, server)
+  manager.setup(server, opts)
+end
+
+-- custom on_attach function that will be used for all the language servers
+-- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+lvim.lsp.on_attach_callback = function(client, bufnr)
+  local function buf_set_option(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
+
+  --Enable completion triggered by <c-x><c-o>
+  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+  if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "helm" then
+    vim.diagnostic.disable(bufnr)
+    vim.defer_fn(function()
+      vim.diagnostic.reset(nil, bufnr)
+    end, 1000)
+  end
+end
--- config/xrandr/laptop-single.layout.sh
+++ config/xrandr/laptop-single.layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1368x768 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-3 --off
+xrandr --output eDP1 --primary --mode 1368x768 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-3 --off
