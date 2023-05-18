6eb6575c32a3f5946da80aa2f769501f88a938ec
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2023-05-18 23:46:42 +0200

    handle auto install

--- config/lvim/lua/nieomylnieja/null-ls.lua
+++ config/lvim/lua/nieomylnieja/null-ls.lua
@@ -1,38 +1,32 @@
 local M = {}
 
 function M.setup()
-  sources = {
+  local sources = {
     linters = {
-      {
-        name = "shellcheck",
-        install = true
-      }
+      { name = "shellcheck" },
+      { name = "ansiblelint" },
     },
     formatters = {
       {
         name = "terraform_fmt",
         filetypes = { "terraform", "tf", "hcl", "terraform-vars" }
       },
-      {
-        name = "taplo",
-        args = { "format", --[[ "--config", "",  ]] "-" },
-        install = true
-      }
+      { name = "taplo", args = { "format", "-" } },
+    },
+    code_actions = {
+      { name = "shellcheck" },
     }
   }
 
   require "lvim.lsp.null-ls.linters".setup(sources.linters)
   require "lvim.lsp.null-ls.formatters".setup(sources.formatters)
+  require "lvim.lsp.null-ls.code_actions".setup(sources.code_actions)
 
-  ensure_installed = {}
-  for _, typ in pairs(sources) do
-    for _, src in ipairs(typ) do
-      if src.install then
-        table.insert(ensure_installed, src.name)
-      end
-    end
-  end
-
+  -- TODO: right now mason-null-ls is lacking... once it fully handles all the renames
+  -- this can be done automatically.
+  local ensure_installed = {
+    "shellcheck", "ansible-lint", "taplo"
+  }
   require "mason-null-ls".setup({ ensure_installed = ensure_installed })
 end
 
