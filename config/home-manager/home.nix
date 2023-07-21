2bcaad3774a921fa578d374b74c79647b32f88af
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2023-07-21 17:10:20 +0200

    cleanup home.nix

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -7,92 +7,92 @@
     username = "mh";
     homeDirectory = "/home/mh";
     stateVersion = "23.05";
+  };
 
-    packages = with pkgs; [
-      apg
-      alacritty
-      arandr
-      bat
-      bash-completion
-      bottom
-      browserpass
-      brave
-      cachix
-      delta
-      docker
-      docker-compose
-      du-dust
-      dunst
-      exa
-      fd
-      flatpak
-      fnm
-      fzf
-      gcc_multi
-      gh
-      git
-      glibcLocales
-      gnupg
-      go
-      httpie
-      jetbrains.goland
-      jq
-      lesspipe
-      libsForQt5.qt5ct
-      luajitPackages.luarocks
-      neofetch
-      neovim
-      nodePackages.npm
-      man
-      man-pages
-      mesa
-      moreutils
-      nitrogen
-      (nerdfonts.override { fonts = ["Mononoki"]; })
-      pamixer
-      pass
-      pavucontrol
-      picom
-      ripgrep
-      ripgrep-all
-      rofi
-      rofi-calc
-      flameshot
-      sops
-      starship
-      statix
-      qtile
-      zoxide
-      xautolock
-      xclip
-      xorg.libXrandr # Required by slock.
-      xorg.xrandr
-      xorg.xset
-      yarn
-      yq
-    ];
+  home.packages = with pkgs; [
+    apg
+    alacritty
+    arandr
+    bat
+    bash-completion
+    bottom
+    browserpass
+    brave
+    cachix
+    delta
+    docker
+    docker-compose
+    du-dust
+    dunst
+    exa
+    fd
+    flatpak
+    fnm
+    fzf
+    gcc_multi
+    gh
+    git
+    glibcLocales
+    gnupg
+    go
+    httpie
+    jetbrains.goland
+    jq
+    lesspipe
+    libsForQt5.qt5ct
+    luajitPackages.luarocks
+    neofetch
+    neovim
+    nodePackages.npm
+    man
+    man-pages
+    mesa
+    moreutils
+    nitrogen
+    (nerdfonts.override { fonts = ["Mononoki"]; })
+    pamixer
+    pass
+    pavucontrol
+    picom
+    ripgrep
+    ripgrep-all
+    rofi
+    rofi-calc
+    flameshot
+    sops
+    starship
+    statix
+    qtile
+    zoxide
+    xautolock
+    xclip
+    xorg.libXrandr # Required by slock.
+    xorg.xrandr
+    xorg.xset
+    yarn
+    yq
+  ];
 
-    file = {
-      ".bashrc".source = ../bash/bashrc;
-      ".bash_logout".source = ../bash/bash_logout;
-      ".xprofile".source = ../xorg/xprofile;
-      ".xinitrc".source = ../xorg/xinitrc;
-      ".profile".source = ../xorg/xinitrc;
-      ".Xresources".source = ../xorg/Xresources;
-      ".config/git/config".source = ../git/config;
-      ".config/starship.toml".source = ../starship/starship.toml;
-      ".config/rofi".source = ../rofi;
-      ".config/lvim".source = ../lvim;
-      ".config/dunst".source = ../dunst;
-      ".config/alacritty".source = ../alacritty;
-      ".config/picom".source = ../picom;
-      ".config/flameshot/flameshot.ini".source = ../flameshot/flameshot.ini;
-      ".config/ideavim".source = ../ideavim;
-    };
+  home.file = {
+    ".bashrc".source = ../bash/bashrc;
+    ".bash_logout".source = ../bash/bash_logout;
+    ".xprofile".source = ../xorg/xprofile;
+    ".xinitrc".source = ../xorg/xinitrc;
+    ".profile".source = ../xorg/xinitrc;
+    ".Xresources".source = ../xorg/Xresources;
   };
 
   xdg.configFile = {
     "qtile".source = ../qtile;
+    "git/config".source = ../git/config;
+    "starship.toml".source = ../starship/starship.toml;
+    "rofi".source = ../rofi;
+    "lvim".source = ../lvim;
+    "dunst".source = ../dunst;
+    "alacritty".source = ../alacritty;
+    "picom".source = ../picom;
+    "flameshot/flameshot.ini".source = ../flameshot/flameshot.ini;
+    "ideavim".source = ../ideavim;
   };
 
   fonts.fontconfig.enable = true;
@@ -106,7 +106,7 @@
 
   programs.browserpass = {
     enable = true;
-    browsers = ["brave"];
+    browsers = ["brave" "firefox"];
   };
 
   services.gpg-agent = {
