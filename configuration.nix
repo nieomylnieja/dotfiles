6a093ba93f702a428297d298c17a42aa1f8d181c
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2023-07-23 22:21:09 +0200

    configuration cleanup

--- configuration.nix
+++ configuration.nix
@@ -37,17 +37,13 @@ in
   };
 
   # Enable swap on luks
-  boot.initrd.luks.devices."luks-${luks-disk}".device = "/dev/disk/by-uuid/${luks-disk}";
-  boot.initrd.luks.devices."luks-${luks-disk}".keyFile = "/crypto_keyfile.bin";
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  boot.initrd.luks.devices."luks-${luks-disk}" = {
+    device = "/dev/disk/by-uuid/${luks-disk}";
+    keyFile = "/crypto_keyfile.bin";
+  };
 
   # Enable networking
+  networking.hostName = "nixos"; # Define your hostname.
   networking.networkmanager.enable = true;
 
   # Set your time zone.
@@ -147,24 +143,8 @@ in
   users.defaultUserShell = pkgs.bash;
   environment.shells = with pkgs; [ bash ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
