6da41e631587e74bac300d46378dedb2e94a68fb
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2023-10-25 19:28:33 +0200

    remove TAB from cmp

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -360,7 +360,7 @@ return {
     config = function()
       require("tabnine").setup({
         disable_auto_comment = true,
-        accept_keymap = "<M-Tab>",
+        accept_keymap = "<Tab>",
         dismiss_keymap = "<C-]>",
         debounce_ms = 800,
         -- suggestion_color = { gui = "#808080", cterm = 244 },
