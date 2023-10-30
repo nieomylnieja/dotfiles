8d475880b2ae0a1dbf09024787508aa114a5bacb
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2023-10-30 19:08:46 +0100

    rollback cmp changes

--- config/nvim/lua/user/cmp.lua
+++ config/nvim/lua/user/cmp.lua
@@ -29,7 +29,7 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ["<Down>"] = cmp.mapping(function(fallback)
+    ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_locally_jumpable() then
@@ -38,7 +38,7 @@ cmp.setup({
         fallback()
       end
     end, { "i", "s" }),
-    ["<Up>"] = cmp.mapping(function(fallback)
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.locally_jumpable(-1) then
