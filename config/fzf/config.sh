d55ecf8419470cc0f163ee34d2944934e84ac583
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2024-01-31 11:21:31 +0100

    fix fzf config

--- config/fzf/config.sh
+++ config/fzf/config.sh
@@ -1,17 +1,19 @@
+#!/usr/bin/env bash
+
 # Bash shared search history
-if [ -f $XDG_CONFIG_HOME/fzf/fzf.bash ]; then
-  source $XDG_CONFIG_HOME/fzf/fzf.bash
+if [ -f "$XDG_CONFIG_HOME/fzf/fzf.bash" ]; then
+	source "$XDG_CONFIG_HOME/fzf/fzf.bash"
 fi
 
 # Source key bindings like CTR+R and completion.
 if command -v fzf-share >/dev/null; then
-  source "$(fzf-share)/key-bindings.bash"
-  source "$(fzf-share)/completion.bash"
+	source "$(fzf-share)/key-bindings.bash"
+	source "$(fzf-share)/completion.bash"
 fi
 
 # Env variables.
 export FZF_DEFAULT_COMMAND='fd --type f'
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'--color fg:#88C0D0,hl:#EBCB8B,fg+:#88C0D0,hl+:#EBCB8B,bg+:#434C5E,pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#B48EAD,marker:#EBCB8B'
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color fg:#88C0D0,hl:#EBCB8B,fg+:#88C0D0,hl+:#EBCB8B,bg+:#434C5E,pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#B48EAD,marker:#EBCB8B"
 
 export FZF_ALT_C_COMMAND='fd --type d'
 export FZF_ALT_C_OPTS='--preview "tree -C {} | head -500"'
